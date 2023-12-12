669536c60c7655133b295e074663a780d2ac9cd3
Sherif-jr <94845939+Sherif-jr@users.noreply.github.com>
2023-12-12 14:55:26 +0200

    static data for preview

---
+++ src/_staticData/basicDetails.ts
@@ -0,0 +1,8 @@
+export const companies: { name: string; path: string }[] = [
+  { name: "MODAD Construction", path: "modad-construction" },
+  { name: "MODAD Properties", path: "modad-properties" },
+  { name: "MODAD MEP", path: "modad-mep" },
+  { name: "MODAD Smart Solutions", path: "modad-smart-solutions" },
+  { name: "MODAD Fine Finishing", path: "modad-fine-finishing" },
+  { name: "MODAD Foundation", path: "modad-foundation" },
+];
