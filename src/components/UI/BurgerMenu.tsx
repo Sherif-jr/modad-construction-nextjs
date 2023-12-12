158923899b7077350f557b46c2c512777145d85c
Sherif-jr <94845939+Sherif-jr@users.noreply.github.com>
2023-12-12 21:35:20 +0200

    feat: added the current menu & styles & improvements

--- src/components/UI/BurgerMenu.tsx
+++ src/components/UI/BurgerMenu.tsx
@@ -3,13 +3,29 @@ import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Drawer } from "antd";
 import Link from "next/link";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import NavLink from "../NavLink";
+import { usePathname } from "next/navigation";
+enum screenSizes {
+  LG = "(max-width: 1024px)",
+}
 const BurgerMenu = () => {
   const [drawerOpen, setDrawer] = useState<boolean>(false);
+  const pathname = usePathname();
   const toggleDrawer = (newState: boolean) => () => {
     setDrawer(newState);
   };
+
+  useEffect(() => {
+    matchMedia(screenSizes.LG).addEventListener("change", (media) => {
+      if (!media.matches) {
+        setDrawer(false);
+      }
+    });
+  }, []);
+  useEffect(() => {
+    setDrawer(false);
+  }, [pathname]);
   return (
     <>
       <button className="ms-6 lg:hidden" onClick={toggleDrawer(true)}>
@@ -26,21 +42,86 @@ const BurgerMenu = () => {
         }}
         closeIcon={<FontAwesomeIcon icon={faX} className="text-white" />}
       >
-        <ul className="flex flex-col gap-4">
-          <li className="text-primary uppercase">
-            <Link href="/">Home</Link>
+        <ul className="flex flex-col gap-4 text-xl">
+          <li className="uppercase">
+            <NavLink
+              href="/"
+              exact
+              className="text-primary hover:text-primary/90 focus:text-primary/90"
+              activeClassName="font-bold"
+            >
+              Home
+            </NavLink>
           </li>
-          <li className="text-primary uppercase">
-            <Link href="/">Divisions</Link>
+          <li className="uppercase">
+            <NavLink
+              href="/about-us"
+              exact
+              className="text-primary hover:text-primary/90 focus:text-primary/90"
+              activeClassName="font-bold"
+            >
+              About us
+            </NavLink>
           </li>
-          <li className="text-primary uppercase">
-            <Link href="/">Highlights</Link>
+          <li className="uppercase">
+            <NavLink
+              href="#"
+              exact
+              className="text-primary hover:text-primary/90 focus:text-primary/90"
+              activeClassName="font-bold"
+            >
+              Our Group
+            </NavLink>
           </li>
-          <li className="text-primary uppercase">
-            <Link href="/">News</Link>
+          <li className="uppercase">
+            <NavLink
+              href="#"
+              exact
+              className="text-primary hover:text-primary/90 focus:text-primary/90"
+              activeClassName="font-bold"
+            >
+              Projects
+            </NavLink>
           </li>
-          <li className="text-primary uppercase">
-            <Link href="/">Corporate Responsibility</Link>
+          <li className="uppercase">
+            <NavLink
+              href="#"
+              exact
+              className="text-primary hover:text-primary/90 focus:text-primary/90"
+              activeClassName="font-bold"
+            >
+              News
+            </NavLink>
+          </li>
+          <li className="uppercase">
+            <NavLink
+              href="#"
+              exact
+              className="text-primary hover:text-primary/90 focus:text-primary/90"
+              activeClassName="font-bold"
+            >
+              Careers
+            </NavLink>
+          </li>
+          <li className="uppercase">
+            <NavLink
+              href="#"
+              exact
+              className="text-primary hover:text-primary/90 focus:text-primary/90"
+              activeClassName="font-bold"
+            >
+              Portals
+            </NavLink>
+          </li>
+          <li className="uppercase">
+            <NavLink
+              href="/contact-us"
+              exact
+              className="text-primary hover:text-primary/90 focus:text-primary/90"
+              activeClassName="font-bold"
+            >
+              Contact Us
+            </NavLink>
           </li>
         </ul>
       </Drawer>
