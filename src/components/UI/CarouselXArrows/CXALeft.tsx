d185450c4b1931599ac58a201d8c59045ac1193e
Sherif-jr <94845939+Sherif-jr@users.noreply.github.com>
2023-12-13 00:54:49 +0200

    fix: updated carousel arrows style

--- src/components/UI/CarouselXArrows/CXALeft.tsx
+++ src/components/UI/CarouselXArrows/CXALeft.tsx
@@ -1,14 +1,20 @@
 "use client";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import ArrowsProps from "../../../interfaces/CarouselArrowProps.interface";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-
+import { Grid } from "antd";
+const { useBreakpoint } = Grid;
 const CXALeft: FC<ArrowsProps> = (props: ArrowsProps) => {
-  const { className, style, onClick, icon, iconClassName, left } = props;
+  const { className, style, onClick, icon, iconClassName, left, xsLeft } =
+    props;
+  const screens = useBreakpoint();
   let leftSpace = left;
   if (leftSpace === undefined) {
     leftSpace = 2;
+  }
+  if (screens.xs && xsLeft !== undefined) {
+    leftSpace = xsLeft;
   }
 
   return (
--- src/components/UI/CarouselXArrows/CXARight.tsx
+++ src/components/UI/CarouselXArrows/CXARight.tsx
@@ -3,12 +3,18 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FC } from "react";
 import ArrowsProps from "../../../interfaces/CarouselArrowProps.interface";
-
+import { Grid } from "antd";
+const { useBreakpoint } = Grid;
 const CXARight: FC<ArrowsProps> = (props: ArrowsProps) => {
-  const { className, style, onClick, icon, iconClassName, right } = props;
+  const { className, style, onClick, icon, iconClassName, right, xsRight } =
+    props;
+  const screens = useBreakpoint();
   let rightSpace = right;
   if (rightSpace === undefined) {
     rightSpace = 98;
+  }
+  if (screens.xs && xsRight !== undefined) {
+    rightSpace = xsRight;
   }
   return (
     <div
