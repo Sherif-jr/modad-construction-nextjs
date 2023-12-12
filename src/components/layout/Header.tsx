a03a7480cb43e78a6074335545b37c3d4ad4a351
Sherif-jr <94845939+Sherif-jr@users.noreply.github.com>
2023-12-12 14:56:18 +0200

    feat: added dropdowns and NavLinks to header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,8 +4,20 @@ import Link from "next/link";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import BurgerMenu from "../UI/BurgerMenu";
+import NavLink from "../NavLink";
+import { Dropdown, MenuProps } from "antd";
+import { companies } from "@/_staticData/basicDetails";
+
+const NewsItems: MenuProps["items"] = [
+  { key: "articles", label: "Article" },
+  { key: "newsletter", label: "Newsletter" },
+];
 
 const Header = () => {
+  const groupItems: MenuProps["items"] = companies.map((company) => ({
+    key: company.path,
+    label: company.name,
+  }));
   return (
     <header className="sticky z-50 top-0 left-0 w-full bg-black text-white h-16 px-4 lg:px-12">
       <div className="flex justify-between h-full items-center">
@@ -15,25 +27,48 @@ const Header = () => {
         <nav className="hidden lg:block">
           <ul className="flex gap-4 text-sm font-bold">
             <li className="uppercase hover:text-gray-300 transition-all">
-              <Link href="/about-us">About us</Link>
+              <NavLink href="/" exact activeClassName="text-primary">
+                Home
+              </NavLink>
             </li>
             <li className="uppercase hover:text-gray-300 transition-all">
-              <Link href="#">Our Services</Link>
+              <NavLink href="/about-us" exact activeClassName="text-primary">
+                About us
+              </NavLink>
             </li>
             <li className="uppercase hover:text-gray-300 transition-all">
-              <Link href="/projects">Our Projects</Link>
+              <Dropdown
+                menu={{
+                  items: groupItems,
+                }}
+              >
+                <span>
+                  <NavLink href="#" exact activeClassName="text-primary">
+                    Our Group
+                  </NavLink>
+                </span>
+              </Dropdown>
             </li>
             <li className="uppercase hover:text-gray-300 transition-all">
-              <Link href="#">Quality Assurance</Link>
+              <NavLink href="/projects" exact activeClassName="text-primary">
+                Projects
+              </NavLink>
             </li>
             <li className="uppercase hover:text-gray-300 transition-all">
-              <Link href="#">Media</Link>
+              <Dropdown menu={{ items: NewsItems }}>
+                <Link href="#">News</Link>
+              </Dropdown>
             </li>
             <li className="uppercase hover:text-gray-300 transition-all">
               <Link href="#">Careers</Link>
             </li>
             <li className="uppercase hover:text-gray-300 transition-all">
-              <Link href="/contact-us">Contact us</Link>
+              <Link href="#">Portals</Link>
+            </li>
+            <li className="uppercase hover:text-gray-300 transition-all">
+              <NavLink href="/contact-us" exact activeClassName="text-primary">
+                Contact us
+              </NavLink>
             </li>
           </ul>
         </nav>
