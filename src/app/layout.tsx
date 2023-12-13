855a69050f9ceef1aaccdbc49eabf35e41334414
Sherif-jr <94845939+Sherif-jr@users.noreply.github.com>
2023-12-13 23:51:01 +0200

    feat: added scroll to top button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import Footer from "@/components/layout/Footer";
 import { ConfigProvider, theme } from "antd";
 import Providers from "@/lib/Providers";
+import ScrollToTop from "@/components/UI/ScrollToTop";
 config.autoAddCss = false;
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Providers>
+          <ScrollToTop />
           <Header />
           {children}
           <Footer />
