9900264b6a40423c849e75658813b5cef6be4ea8
Sherif-jr <94845939+Sherif-jr@users.noreply.github.com>
2023-12-17 16:15:02 +0200

    Update basicDetails.ts

--- src/_staticData/basicDetails.ts
+++ src/_staticData/basicDetails.ts
@@ -6,3 +6,17 @@ export const companies: { name: string; path: string }[] = [
   { name: "MODAD Fine Finishing", path: "modad-fine-finishing" },
   { name: "MODAD Foundation", path: "modad-foundation" },
 ];
+
+export const projects = [
+  { name: "Title 1", brand: "MODAD Construction", scope: "Scope 1" },
+  { name: "Title 2", brand: "MODAD Construction", scope: "Scope 2" },
+  { name: "Title 3", brand: "MODAD Construction", scope: "Scope 3" },
+  { name: "Title 4", brand: "MODAD Construction", scope: "Scope 4" },
+  { name: "Title 5", brand: "MODAD Construction", scope: "Scope 1" },
+  { name: "Title 6", brand: "MODAD Construction", scope: "Scope 2" },
+  { name: "Title 7", brand: "MODAD Construction", scope: "Scope 1" },
+  { name: "Title 8", brand: "MODAD Construction", scope: "Scope 2" },
+  { name: "Title 9", brand: "MODAD Construction", scope: "Scope 3" },
+  { name: "Title 10", brand: "MODAD Construction", scope: "Scope 1" },
+  { name: "Title 11", brand: "MODAD Construction", scope: "Scope 2" },
+];
