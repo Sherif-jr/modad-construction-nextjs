3c8084973df59b0ec77677f890f98e0a4c0742b4
Sherif-jr <94845939+Sherif-jr@users.noreply.github.com>
2023-12-17 16:49:01 +0200

    Update basicDetails.ts

--- src/_staticData/basicDetails.ts
+++ src/_staticData/basicDetails.ts
@@ -8,15 +8,70 @@ export const companies: { name: string; path: string }[] = [
 ];
 
 export const projects = [
-  { name: "Title 1", brand: "MODAD Construction", scope: "Scope 1" },
-  { name: "Title 2", brand: "MODAD Construction", scope: "Scope 2" },
-  { name: "Title 3", brand: "MODAD Construction", scope: "Scope 3" },
-  { name: "Title 4", brand: "MODAD Construction", scope: "Scope 4" },
-  { name: "Title 5", brand: "MODAD Construction", scope: "Scope 1" },
-  { name: "Title 6", brand: "MODAD Construction", scope: "Scope 2" },
-  { name: "Title 7", brand: "MODAD Construction", scope: "Scope 1" },
-  { name: "Title 8", brand: "MODAD Construction", scope: "Scope 2" },
-  { name: "Title 9", brand: "MODAD Construction", scope: "Scope 3" },
-  { name: "Title 10", brand: "MODAD Construction", scope: "Scope 1" },
-  { name: "Title 11", brand: "MODAD Construction", scope: "Scope 2" },
+  {
+    id: "title-1",
+    name: "Title 1",
+    brand: "MODAD Construction",
+    scope: "Scope 1",
+  },
+  {
+    id: "title-2",
+    name: "Title 2",
+    brand: "MODAD Construction",
+    scope: "Scope 2",
+  },
+  {
+    id: "title-3",
+    name: "Title 3",
+    brand: "MODAD Construction",
+    scope: "Scope 3",
+  },
+  {
+    id: "title-4",
+    name: "Title 4",
+    brand: "MODAD Construction",
+    scope: "Scope 4",
+  },
+  {
+    id: "title-5",
+    name: "Title 5",
+    brand: "MODAD Construction",
+    scope: "Scope 1",
+  },
+  {
+    id: "title-6",
+    name: "Title 6",
+    brand: "MODAD Construction",
+    scope: "Scope 2",
+  },
+  {
+    id: "title-7",
+    name: "Title 7",
+    brand: "MODAD Construction",
+    scope: "Scope 1",
+  },
+  {
+    id: "title-8",
+    name: "Title 8",
+    brand: "MODAD Construction",
+    scope: "Scope 2",
+  },
+  {
+    id: "title-9",
+    name: "Title 9",
+    brand: "MODAD Construction",
+    scope: "Scope 3",
+  },
+  {
+    id: "title-10",
+    name: "Title 10",
+    brand: "MODAD Construction",
+    scope: "Scope 1",
+  },
+  {
+    id: "title-11",
+    name: "Title 11",
+    brand: "MODAD Construction",
+    scope: "Scope 2",
+  },
 ];
